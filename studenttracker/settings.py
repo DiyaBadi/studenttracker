0b6034c6c3764e3bd947fda94b628c1fb851ff70
Diya Badi <diyabaddi14@gmail.com>
2026-04-09 11:07:00 +0530

    fixed email config for render

--- studenttracker/settings.py
+++ studenttracker/settings.py
@@ -122,12 +122,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "tracker/static",]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
+# Email configuration using environment variables
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "diyabaddi14@gmail.com"
-EMAIL_HOST_PASSWORD = "bncj kvel cfia tcqx"
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
